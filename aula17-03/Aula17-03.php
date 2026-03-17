765534b37a1c9226f20002af94946630406fce98
Pedro <nicolucipedro@gmail.com>
2026-03-17 20:29:00 -0300

    exercicio 1 lista 3 exercicio 1 lista 1 git config

---
+++ aula17-03/Aula17-03.php
@@ -0,0 +1,35 @@
+<?php
+    $mapa1 = array('João', 'Maria', 3);
+    print_r($mapa1);
+    echo"<p></p>";
+    var_dump($mapa1);
+    echo "<p>Valor da posição 2 ".$mapa1[2]."</p>";
+
+    $mapa2[1] = "Vanessa";
+    $mapa2[2] = "José";
+    $mapa2[3] = "Clara";
+    print_r($mapa2);
+
+    $contatos["Vanessa"] = "12345";
+    $contatos["José"] = "09876";
+    echo "<p></p>";
+    print_r($contatos);
+
+    foreach($contatos as $valor){
+        echo "<p>Telefone: $valor </p>";
+    }
+
+    foreach($contatos as $chave => $valor){
+        echo"<p>Telefone de $chave: $valor </p>";
+    }
+
+    //apagar posição
+    unset($mapa1[2]);
+    print_r($mapa1);
+
+    //funções
+    $quantidade = count($mapa2);
+    echo "<p>Qtde. de elementos mapa 2: $quantidade</P>";
+    asort($contatos); //ordenar pel valor
+    ksort($contatos); //ordenar pela chave
+?>
