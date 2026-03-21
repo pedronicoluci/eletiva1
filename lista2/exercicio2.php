71d3b4e8b82136e3a4cdd02f399bbf7c19e7f7e9
Pedro <nicolucipedro@gmail.com>
2026-03-20 21:31:55 -0300

    fiz o exercício 2 da lista 2

---
+++ lista2/exercicio2.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercício 2</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container py-3">
+        <h1>Exercício 2</h1>
+        <form method="post">
+            <div class="mb-3">
+                <label for="numero1" class="form-label">Digite um número:</label>
+                <input type="number" id="numero1" name="numero1" class="form-control" required="">
+            </div>
+            <div class="mb-3">
+                <label for="numero2" class="form-label">Digite outro número:</label>
+                <input type="number" id="numero2" name="numero2" class="form-control" required="">
+            </div>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+
+        <?php
+            if($_SERVER["REQUEST_METHOD"] == "POST"){
+                $numero1 = $_POST["numero1"];
+                $numero2 = $_POST["numero2"];
+                $result = $numero1 - $numero2;
+                echo "<p>O resultado da subtração é igual a: $result</p>";
+            }
+
+        ?>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+    </div>
+</body>
+
+</html>
