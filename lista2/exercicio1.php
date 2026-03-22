89d882f10efb3389cc7b61370000fc01780ce56d
Pedro <nicolucipedro@gmail.com>
2026-03-22 18:46:47 -0300

    fiz o exercício 8

--- lista2/exercicio1.php
+++ lista2/exercicio1.php
@@ -4,13 +4,13 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Somar 2 Números</title>
+  <title>Exercício 1</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
   <div class="container py-3">
-    <h1>Somar 2 Números</h1>
+    <h1>Exercício 1</h1>
     <form method="post">
       <div class="mb-3">
         <label for="numero1" class="form-label">Digite um número:</label>
