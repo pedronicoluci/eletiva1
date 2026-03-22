d4fef25ff393bf2e8709e84f864ec491994b9831
Pedro <nicolucipedro@gmail.com>
2026-03-22 19:27:22 -0300

    fiz o exercício 13

---
+++ lista2/exercicio13.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercício 13</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container py-3">
+        <h1>Exercício 13</h1>
+        <form method="post">
+            <div class="mb-3">
+                <label for="metros" class="form-label">Digite um valor em metros:</label>
+                <input type="number" id="met" name="met" class="form-control" required="">
+            </div>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $met = $_POST["met"];
+            $cent = $met * 100;
+            echo "<p>$met metro(s) convertido para centímetros é igual a $cent centímetros.</p>";
+        }
+        ?>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO"
+            crossorigin="anonymous"></script>
+    </div>
+</body>
+
+</html>
