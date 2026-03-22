277cf186b946ec5130e1a950a6da5d62151580bc
Pedro <nicolucipedro@gmail.com>
2026-03-22 18:12:12 -0300

    fiz o exercício 5

---
+++ lista2/exercicio5.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercício 3</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container py-3">
+        <h1>Exercício 3</h1>
+        <form method="post">
+            <div class="mb-3">
+                <label for="nota1" class="form-label">Digite a primeira nota:</label>
+                <input type="number" id="nota1" name="nota1" class="form-control" required="">
+            </div>
+            <div class="mb-3">
+                <label for="nota2" class="form-label">Digite a segunda nota:</label>
+                <input type="number" id="nota2" name="nota2" class="form-control" required="">
+            </div>
+            <div class="mb-3">
+                <label for="nota3" class="form-label">Digite a terceira nota:</label>
+                <input type="number" id="nota3" name="nota3" class="form-control" required="">
+            </div>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $nota1 = $_POST["nota1"];
+            $nota2 = $_POST["nota2"];
+            $nota3 = $_POST["nota3"];
+            $result = ($nota1 + $nota2 + $nota3) / 3;
+            echo "<p>A média das notas é igual a: $result</p>";
+        }
+        ?>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO"
+            crossorigin="anonymous"></script>
+    </div>
+</body>
+
+</html>
