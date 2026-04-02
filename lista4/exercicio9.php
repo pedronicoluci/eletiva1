91026726f9d81bc2f83057ad33a0a3fd8ab380f4
Pedro <nicolucipedro@gmail.com>
2026-04-01 21:38:40 -0300

    fiz o exercício 9

---
+++ lista4/exercicio9.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Exercício 9</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
+<h1>Exercício 9</h1>
+<form method="post">
+<div class="mb-3">
+              <label for="frase" class="form-label">Digite uma palavra:</label>
+              <input type="text" id="frase" name="frase" class="form-control" required="">
+            </div>
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+
+<?php
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $frase = $_POST['frase'];
+        $fraseSEspacoEx = trim($frase);
+        $fraseSEspaco = str_replace(" ","",$frase);
+
+        echo "<p>Original: '$frase'.</p>";
+        echo "<p>Sem espaços extras no início e no fim: '$fraseSEspacoEx'.</p>";
+        echo "<p>Sem espaços: '$fraseSEspaco'.</p>";
+    }
+?>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
